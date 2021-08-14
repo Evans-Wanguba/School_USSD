efd48b4d484935bd4ef96bfc8be30e5d413c2d9e
Evans-Wanguba <ewanguba@gmail.com>
2021-08-14 17:57:14 +0300

    second commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+//This is the config.php file
+define('CLIENT_ID','**********************************************');
+define('CLIENT_SECRET','*************************');
+define('CALLBACK_URL','https://localhost/callback');
+define('CACHE_DIR', 'cache');
+define('LOGIN_URL','https://localhost/login');
+define('USERNAME','**********************');
+define('PASSWORD','*************************');
+
+
+?>
